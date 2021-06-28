8d5f25088a3d63e79ab7a6cf43c0d927a830c6b2
lbergengruen <lbergengruen@sessionm.com>
2021-06-28 13:49:59 -0300

    Change

--- demo/take_images.py
+++ demo/take_images.py
@@ -31,6 +31,7 @@ if __name__ == "__main__":
         for idx in [0, 1]:
             stream = cameras[idx]
             frame = stream.read()
+            print(frame.shape)
             frame = imutils.resize(frame, height=240, width=320)
             images.append(cv2.rotate(frame, rotations[idx]))
 
